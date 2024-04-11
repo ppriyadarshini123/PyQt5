77c0fecd236ef2d478f724c0057b615820aa242b
ppriyadarshini123 <payal.priyadarshini@gmail.com>
2024-04-11 20:10:52 +0100

    First Commit

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import sys
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
+        #self.setGeometry(xpos, ypos, width, height)
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("My first PyQt file")  
         self.initUI()
     
     def initUI(self):
@@ -17,17 +20,20 @@ class MyWindow(QMainWindow):
         #Button
         self.b1=QtWidgets.QPushButton(self)
         self.b1.setText("Click me")
-        self.b1.clicked.connect(clicked)
+        self.b1.clicked.connect(self.clicked)
 
     def clicked(self):
         self.label.setText("You pressed the button")
+        self.update()
+    
+    def update(self):
+        #Adjusts the size of the label
+        self.label.adjustSize()
 
 def window():
     app = QApplication(sys.argv)
-    win=QMainWindow()
-    #win.setGeometry(xpos, ypos, width, height)
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("My first PyQt file")    
+    win=MyWindow()
+   
 
     win.show()
     sys.exit(app.exec_())
